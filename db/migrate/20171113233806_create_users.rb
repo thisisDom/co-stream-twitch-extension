e18e087bd6bce37a29ff432059a3ed581b4608e9
Dominique Crowther <Dominique.Crowther@gmail.com>
2017-11-13 22:54:58 -0800

    adds user table to schema

--- db/migrate/20171113233806_create_users.rb
+++ db/migrate/20171113233806_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
+      t.string :username, null: false
+      t.string :twitch_id, null: false
       t.timestamps
     end
   end
